7383d3df0411c96880267d44e126c2f3cad2ae2c
Džemil Manić <dzemilmanic@hotmail.com>
2025-07-05 15:53:18 +0200

    build vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import '../css/home.css';
 import '../css/login.css';
 import '../css/profile.css';
 import '../css/auth.css';
+import '../css/sweetalert-global.css';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
